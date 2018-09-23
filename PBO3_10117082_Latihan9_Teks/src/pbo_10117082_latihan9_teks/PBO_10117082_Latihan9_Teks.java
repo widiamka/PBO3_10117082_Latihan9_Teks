ff3dc80dbbc0d48a707a32c511c21bcca4f517a7
widiamka <43339745+widiamka@users.noreply.github.com>
2018-09-23 10:21:21 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan9_Teks/src/pbo_10117082_latihan9_teks/PBO_10117082_Latihan9_Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_10117082_latihan9_teks;
+
+/**
+ *
+ * @author nama : WIDIAMEGA K
+ *          Kelas : IF3
+ *          NIM     : 10117082
+ */
+public class PBO_10117082_Latihan9_Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char:" + tab + "Berisi " + c);
+    
+    
+    
+    }
+    
+}
